16253c372518d13c6b79e0bf8c23b7137a1c35bb
Kenean Dita <KeneanSufa@gmail.com>
2025-06-16 14:16:12 +0300

    Demo Elements and editable tables

--- Streamlit/stream.py
+++ Streamlit/stream.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import os 
+import os
+import pandas as pd
 
 st.title("Streamlit Crash Course")
 st.header("Welcome to Streamlit")
@@ -29,3 +30,28 @@ print("Second:",pressed2)
 st.divider()
 
 st.image(os.path.join(os.getcwd(), "static", '1.jpg'), caption='Sample Image', use_container_width=True)
+
+st.divider()
+
+
+
+st.title("Streamlit Elements Demo")
+# Dataframe Section
+st.subheader("Dataframe")
+
+df = pd.DataFrame({
+    'Name': ['Alice', 'Bob', 'Charlie', 'David'],
+    'Age': [25, 32, 37, 45],
+    'Occupation': ['Engineer', 'Doctor', 'Artist', 'Chef']
+})
+
+st.dataframe(df)
+# Data Editor Section (Editable dataframe)
+st.subheader("Data Editor")
+editable_df = st.data_editor(df)
+
+
+# Static Table Section
+st.subheader("Static Table")
+
+st.table(df)
