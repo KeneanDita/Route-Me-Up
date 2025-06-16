d757bbdd27608a9c74c0c7e27bc14e9d74eabdf9
Kenean Dita <KeneanSufa@gmail.com>
2025-06-16 14:00:17 +0300

    Buttons and state changes

--- Streamlit/stream.py
+++ Streamlit/stream.py
@@ -2,3 +2,16 @@ import streamlit as st
 import os 
 
 st.write("Hello, Streamlit!")
+st.write("Current working directory:", os.getcwd())
+
+if 1 + 3 == 4:
+    st.write("1 + 3 equals 4")
+else:
+    st.write("1 + 3 does not equal 4")
+
+
+pressed = st.button("Press me")
+print("first:",pressed)
+
+pressed2 = st.button("Sec me")
+print("Second:",pressed2)
