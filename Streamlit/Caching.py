f921844ef304e01367218e28ed809710b9ee2f68
Kenean Dita <KeneanSufa@gmail.com>
2025-06-17 15:23:34 +0300

    Caching resource

--- Streamlit/Caching.py
+++ Streamlit/Caching.py
@@ -1,11 +1,23 @@
-import streamlit as st  
-import time
+import streamlit as st
 
-@st.cache_data(ttl=60)
-def fetch_data():
-    time.sleep(3)
-    return {"Data" : "Data fetched successfully!"}
+file_path = "static/ff.txt"
 
-st.write("Fetching data...")
-data = fetch_data()
-st.write(data)
+@st.cache_resource
+def get_file_handler():
+    flie = open(file_path, "a+")
+    return flie
+
+file_handler = get_file_handler()
+
+if st.button("Write to File"):
+    file_handler.write("Hello, Streamlit!\n")
+    file_handler.flush()
+    st.success("Data written to file successfully!")
+    
+if st.button("Read from File"):
+    file_handler.seek(0)  # Move the cursor to the beginning of the file
+    content = file_handler.read()
+    st.text_area("File Content", content, height=300)
+    
+st.button("Close File", on_click=file_handler.close)
+st.write("File handler is cached and will not be closed until the app stops.")
