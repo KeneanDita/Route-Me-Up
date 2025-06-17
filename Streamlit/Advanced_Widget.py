5487ac15f06bbf6f089057887f0c475c0fd416af
Kenean Dita <KeneanSufa@gmail.com>
2025-06-17 14:27:44 +0300

    session state slider

--- Streamlit/Advanced_Widget.py
+++ Streamlit/Advanced_Widget.py
@@ -4,3 +4,12 @@ st.title("Advanced Widgets Example")
 
 st.button("Ok", key="1")
 st.button("Ok", key="2")
+
+# if "slider" not in st.session_state:
+#     st.session_state.slider = 25
+
+min_value = st.slider(
+    "Select a value",0, 100, 25)
+
+st.session_state.slider = st.slider(
+    "Select a value", min_value, 100, st.session_state.slider)
