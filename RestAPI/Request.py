60dce21fd16bd04c22164f159012980728d3ae2c
Kenean Dita <KeneanSufa@gmail.com>
2025-08-24 11:10:34 +0300

    refactor: update API response handling to print question titles
    Prints question titles from API response
    
    Refactors API response handling to iterate over the "items"
    array and print the "title" of each question. Improves
    output readability by focusing on relevant data.

--- RestAPI/Request.py
+++ RestAPI/Request.py
@@ -5,4 +5,5 @@ response = requests.get(
     "https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow"
 )
 
-print(response.json())
+for data in response.json()["items"]:
+    print(data["title"])
