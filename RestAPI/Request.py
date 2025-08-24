ea581fdc589e4293beaab9a8856e0907a37ac8e8
Kenean Dita <KeneanSufa@gmail.com>
2025-08-24 09:57:59 +0300

    refactor: simplify API request handling and remove unused DataFrame conversion

--- RestAPI/Request.py
+++ RestAPI/Request.py
@@ -1,21 +1,8 @@
 import requests
-import pandas as pd
+import json
 
-# Get data from API
 response = requests.get(
     "https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow"
 )
 
-# Convert response to Python dict
-data = response.json()
-
-# The questions are inside the "items" key
-questions = data["items"]
-
-# Convert to DataFrame
-df = pd.DataFrame(questions)
-
-# Save to JSON file
-df.to_json("stackoverflow_questions.json", orient="records", indent=4)
-
-print("Saved data to stackoverflow_questions.json")
+print(response.json())
