60c5ee12ebe065c74c0b0d57d37b03f08704f656
Kenean Dita <KeneanSufa@gmail.com>
2025-09-03 10:48:17 +0300

    grayscaling images

--- Open CV/opencv.py
+++ Open CV/opencv.py
@@ -1,9 +1,15 @@
 import cv2 as cv
 
 img = cv.imread("Open CV/assets/1.jpg")
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # convert to grayscale
 
 if img is None:
     raise FileNotFoundError(f"Image not found.")
+else:
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    cv.imshow("Gray Image", gray)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 cv.imshow("Mustang", img)
 cv.waitKey(0)
